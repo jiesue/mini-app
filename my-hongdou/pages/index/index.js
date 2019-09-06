ec90550023f21a1970b6678366d9c87561d3cea4
jiesue <934164066@qq.com>
2019-09-07 00:47:53 +0800

    1

--- my-hongdou/pages/index/index.js
+++ my-hongdou/pages/index/index.js
@@ -9,7 +9,9 @@ Page({
         hasUserInfo: false,
         activeIndex: 0,
         tabItems: ['关注', '直播', '语聊'],
-        listItems: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        list1Items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        list2Items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        list3Items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         // canIUse: wx.canIUse('button.open-type.getUserInfo')
     },
     change(e) {
@@ -17,10 +19,18 @@ Page({
             activeIndex: e.currentTarget.dataset.index
         })
     },
+    swiperChange(e) {
+        
+        if (e.detail.source == 'touch') {//滑动的时候执行改变tab
+            this.setData({
+                activeIndex: e.detail.current
+            })
+        }
+    },
     search() {
         console.log('search')
     },
-    onLoad: function () {
+    onLoad: function() {
         console.log(util.checkLogin())
         // if (app.globalData.userInfo) {
         //     this.setData({
